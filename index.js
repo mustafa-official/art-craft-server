2c374105cad6764d0df79f5f509fdded020b161a
webdev-mostofa <mdmostofaa24@gmail.com>
2024-04-27 16:40:09 +0600

    dynamically select the user email and get the data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,7 +15,7 @@ app.use(express.json());
 //mongodb
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.elzgrcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -29,6 +29,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const artCollection = client.db('artDB').collection('art');
+
+    //get craft
+    app.get("/craft", async (req, res) => {
+      const cursor = artCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    //specific craft
+    app.get("/craft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artCollection.findOne(query);
+      res.send(result)
+    })
+    //my added craft item
+    app.get("/myCraft/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await artCollection.find({ email: req.params.email }).toArray();
+      res.send(result)
+    })
+
+    //create craft
+    app.post("/craft", async (req, res) => {
+      const craftItem = req.body;
+      const result = await artCollection.insertOne(craftItem);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -40,10 +70,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('Art and Craft is Running')
+app.get('/', (req, res) => {
+  res.send('Art and Craft is Running')
 })
 
-app.listen(port, ()=>{
-    console.log(`Running port on ${port}`);
+app.listen(port, () => {
+  console.log(`Running port on ${port}`);
 })
