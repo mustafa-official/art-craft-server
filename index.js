153cb8a00ffef82d44206f30c7f81b78a5c8b983
webdev-mostofa <mdmostofaa24@gmail.com>
2024-04-28 14:31:17 +0600

    for filter implement to customization

--- index.js
+++ index.js
@@ -44,11 +44,48 @@ async function run() {
       const result = await artCollection.findOne(query);
       res.send(result)
     })
+    //find customization
+    app.get("/myValue/:customization", async (req, res) => {
+      const result = await artCollection.find({ customization: req.params.customization }).toArray();
+      res.send(result);
+    })
     //my added craft item
     app.get("/myCraft/:email", async (req, res) => {
       console.log(req.params.email);
       const result = await artCollection.find({ email: req.params.email }).toArray();
       res.send(result)
+    })
+
+    //update myCraft
+    app.put("/craft/:id", async (req, res) => {
+      const id = req.params.id;
+      const craft = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateCraft = {
+        $set: {
+          userName: craft.userName,
+          itemName: craft.itemName,
+          subcategory: craft.subcategory,
+          price: craft.price,
+          rating: craft.rating,
+          processTime: craft.processTime,
+          image: craft.image,
+          customization: craft.customization,
+          stockStatus: craft.stockStatus,
+          description: craft.description,
+        }
+      }
+      const result = await artCollection.updateOne(filter, updateCraft, options);
+      res.send(result);
+    })
+
+    //delete craft
+    app.delete("/craft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artCollection.deleteOne(query);
+      res.send(result);
     })
 
     //create craft
@@ -57,6 +94,8 @@ async function run() {
       const result = await artCollection.insertOne(craftItem);
       res.send(result);
     })
+
+
 
 
     // Send a ping to confirm a successful connection
